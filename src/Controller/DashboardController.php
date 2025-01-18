4e3b8a4cf6349b61027a978a64fccae614605851
Edric Huang <Edric Huang>
2025-01-18 17:18:29 +0100

    Added default code to display the dashboard view in DashboardController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -36,3 +36,31 @@ class DashboardController extends AbstractController
         ]);
     }
 }
+
+// DashboardController par défaut (qui permet de voir la vue dashboard)
+// <?php
+
+// namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Attribute\Route;
+
+// final class DashboardController extends AbstractController
+// {
+//     #[Route('/dashboard', name: 'app_dashboard')]
+//     public function index(): Response
+//     {
+//         return $this->render('dashboard/user_dashboard.html.twig', [
+//             'controller_name' => 'DashboardController',
+//         ]);
+//     }
+
+//     #[Route('/dashboard/admin', name: 'app_dashboard_admin')]
+//     public function admin(): Response
+//     {
+//         return $this->render('dashboard/admin_dashboard.html.twig', [
+//             'controller_name' => 'DashboardController',
+//         ]);
+//     }
+// }
